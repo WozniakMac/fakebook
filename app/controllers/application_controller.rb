6441747ba94619b7e99d114e267d2133a8cd0693
Maciej Woźniak <WozniakMac@users.noreply.github.com>
2018-05-28 17:27:38 +0100

    Login by Google (#1)
    * Add google oauth authentication.
    
    * Fix travis tests.
    
    * Fix travis tests db.
    
    * Fix travis tests db back.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def authenticate
+    redirect_to :login unless user_logged_in?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_logged_in?
+    current_user.present?
+  end
+
+  def validate_author(object)
+    redirect_back(fallback_location: root_path) unless current_user == object.user
+  end
 end
